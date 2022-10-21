9e576d14882668a91f2dffdce9041af609cb7f9e
MaxwellGengYF <maxwellgeng@outlook.com>
2022-10-21 14:55:44 +0800

    test ssh commit

--- src/tests/xmake.lua
+++ src/tests/xmake.lua
@@ -3,5 +3,7 @@ BuildProject({
 	projectType = "binary"
 })
 add_includedirs("../ext/stb/")
-add_files("test_dispatch_indirect.cpp", "../ext/stb/stb.c")
+-- add_files("test_rtx.cpp", "../ext/stb/stb.c")
+-- add_files("test_raster.cpp", "../ext/stb/stb.c")
+add_files("test_dispatch_indirect.cpp")
 add_deps("luisa-compute-dsl", "luisa-compute-runtime", "luisa-compute-vstl")
